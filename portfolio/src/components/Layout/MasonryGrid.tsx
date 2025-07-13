13304922a24640b87563ef0007fb367dc3c93b8e
Arley <aguerrero0615@gmail.com>
2025-07-12 19:38:31 -0500

    Feat: Se organiza el home

--- portfolio/src/components/Layout/MasonryGrid.tsx
+++ portfolio/src/components/Layout/MasonryGrid.tsx
@@ -1,20 +1,36 @@
 import React from "react";
 import styles from "./MasonryGrid.module.css";
 
+interface MasonryImage {
+  image: string;
+  url?: string;
+}
+
 interface MasonryGridProps {
-  images: string[];
+  images: (string | MasonryImage)[];
 }
 
 const MasonryGrid: React.FC<MasonryGridProps> = ({ images }) => {
   return (
     <div className={styles.masonryGrid}>
-      {images.map((src, idx) => (
-        <div className={styles.masonryItem} key={idx}>
-          <img src={src} alt={`Imagen ${idx + 1}`} />
-        </div>
-      ))}
+      {images.map((img, idx) => {
+        const src = typeof img === "string" ? img : img.image;
+        const url = typeof img === "string" ? undefined : img.url;
+        const imgTag = <img src={src} alt={`Imagen ${idx + 1}`} />;
+        return (
+          <div className={styles.masonryItem} key={idx}>
+            {url ? (
+              <a href={url} target="_blank" rel="noopener noreferrer">
+                {imgTag}
+              </a>
+            ) : (
+              imgTag
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 };
 
-export default MasonryGrid; 
+export default MasonryGrid;
